a59e9f31839811d5bf36a1471a250a19101e5786
mattwoodruff808 <mjwoodruff93@gmail.com>
2020-12-16 15:28:45 -0700

    Did app-3 twice through!

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      arr: ['Shards', 'Investiture', 'Connection', 'Intent', 'Spren', 'Metals', 'Shardblades', 'Dogs', 'Moon Scepter'],
+      userInput: ''
+    }
+  }
+
+  handleChange(e){
+    this.setState({userInput: e.target.value})
+  }
+
+  render(){
+    let listDisplay = this.state.arr
+      .filter((item) => {
+        return item.includes(this.state.userInput)
+      })
+      .map((item, i) => {
+        return <h2 key={i}>{item}</h2>
+      })
+
+    return (
+      <div className="App">
+        <input onChange={(e) => this.handleChange(e)}/>
+        {listDisplay}
+      </div>
+    )
+  }
 }
 
 export default App;
